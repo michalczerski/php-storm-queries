a770e357eaa33ddd02beacb1728a84296201fb2c
Michał Czerski <czerski.michal@gmail.com>
2024-12-30 16:08:10 +0100

    mariadb

--- tests/mariadb.php
+++ tests/mariadb.php
@@ -4,9 +4,9 @@ require_once 'autoload.php';
 
 use data\ConnectionProvider;
 
-const CONNECTION_USER = "sa";
-const CONNECTION_PASS = "Sqlserver123";
-const CONNECTION_STRING = "sqlsrv:server=localhost,7803";
+const CONNECTION_USER = "root";
+const CONNECTION_PASS = "mariadb";
+const CONNECTION_STRING = "mysql:host=localhost;port=7802;";
 
 
 $connection = ConnectionProvider::getConnection();
@@ -14,9 +14,8 @@ $connection->execute("DROP DATABASE IF EXISTS storm_test");
 $connection->execute("CREATE DATABASE storm_test");
 $connection->execute("USE storm_test");
 
-$schema = file_get_contents(__DIR__ . "/data/sqlserver.sql");
+$schema = file_get_contents(__DIR__ . "/data/mariadb.sql");
 $data = file_get_contents(__DIR__ . "/data/data.sql");
 
 $connection->executeCommands($schema);
 $connection->executeCommands($data);
-
